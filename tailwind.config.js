262647893d415f3e2934c4f6aaa1be856d3d3ba4
liaidm19-fbank <liaidm19@gmail.com>
2026-01-11 17:20:52 -0500

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
-        "./app/**/*.php",
-        "./storage/framework/views/*.php",
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-};
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./app/Livewire/**/*.php",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
